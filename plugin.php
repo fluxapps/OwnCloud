e29e639edf1f395119c3d0871480e13f872848fb
Benjamin Seglias <bs@studer-raimann.ch>
2018-04-30 10:00:17 +0200

    updated plugin version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'owncld';
 $version = '2.0.4';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
