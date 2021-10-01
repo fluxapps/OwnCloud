a70ad932577872301454992d20780c9683fa9157
Theodor Truffer <theo@fluxlabs.ch>
2021-10-01 14:56:56 +0200

    ilias7

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'owncld';
 $version = '2.7.3';
 $ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
+$ilias_max_version = '7.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
